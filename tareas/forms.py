6ccc9db90a06053b343136d3255f6ee45a4f10d6
jaime-max <jaime.paqui@unl.edu.ec>
2023-09-12 11:26:46 -0500

    App terminada

--- tareas/forms.py
+++ tareas/forms.py
@@ -1,7 +1,13 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Tareas
 
-class FormularioTareas(ModelForm):
+class FormularioTareas(forms.ModelForm):
     class Meta:
         model = Tareas
         fields = ['titulo','descripcion','importante']
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class':'form-control','placeholder': 'Ingrese un titulo'}),
+            'descripcion': forms.Textarea(attrs={'class':'form-control','placeholder': 'Ingrese una descripcion'}),
+            'importante': forms.CheckboxInput(attrs={'class':'form-check-input text-center'}),
+            #m-auto
+        }
